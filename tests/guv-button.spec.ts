0c33a6f09293473d99f27e00774e4cb3389d3424
Garrison <garrison@pc103895.PKRVMN.local>
2023-11-10 16:17:54 +0300

    test: refactor Button

--- tests/guv-button.spec.ts
+++ tests/guv-button.spec.ts
@@ -7,98 +7,96 @@ describe('Button', () => {
    * Positive testing
    */
   it('Title -> test', () => {
+    expect(Button).toBeTruthy();
+
     const wrapper = mount(Button, {
       props: {
         title: 'Click me',
       },
     });
-
-    expect(wrapper).toBeTruthy();
     expect(wrapper.html()).toContain('Click me');
   });
 
   it('Title -> undefined', () => {
-    const wrapper = mount(Button);
+    expect(Button).toBeTruthy();
 
-    expect(wrapper).toBeTruthy();
+    const wrapper = mount(Button);
     expect(wrapper.html()).toContain('');
   });
 
   it('Slots', () => {
+    expect(Button).toBeTruthy();
+
     const wrapper = mount(Button, {
       slots: {
         before: 'Before',
         after: '<div>After</div>',
       },
     });
-
-    expect(wrapper).toBeTruthy();
     expect(wrapper.html()).toContain('Before');
     expect(wrapper.html()).toContain('<div>After</div>');
   });
 
   it('Classes -> main', () => {
-    const wrapper = mount(Button);
+    expect(Button).toBeTruthy();
 
-    expect(wrapper).toBeTruthy();
+    const wrapper = mount(Button);
     expect(wrapper.classes()).toContain('guv-button');
   });
 
   it('Classes -> test', () => {
+    expect(Button).toBeTruthy();
+
     const wrapper = mount(Button, {
       props: {
         class: 'test',
       },
     });
-
-    expect(wrapper).toBeTruthy();
     expect(wrapper.classes()).toContain('test');
   });
 
   it('Style -> add', () => {
+    expect(Button).toBeTruthy();
+
     const wrapper = mount(Button, {
       props: {
         style: { paddingTop: '10px' },
       },
     });
-
-    expect(wrapper).toBeTruthy();
     expect(wrapper.attributes().style).toContain('padding-top: 10px;');
   });
 
   it('Emit -> mounted', () => {
-    const wrapper = mount(Button);
+    expect(Button).toBeTruthy();
 
-    expect(wrapper).toBeTruthy();
+    const wrapper = mount(Button);
     expect(wrapper.emitted()).toHaveProperty('mount');
   });
 
   it('Emit -> unmounted', () => {
+    expect(Button).toBeTruthy();
+
     const wrapper = mount(Button);
-
-    expect(wrapper).toBeTruthy();
-
     wrapper.unmount();
     expect(wrapper.emitted()).toHaveProperty('unmount');
   });
 
   it('Emit -> click', () => {
+    expect(Button).toBeTruthy();
+
     const wrapper = mount(Button);
-
-    expect(wrapper).toBeTruthy();
-
     wrapper.trigger('click');
     expect(wrapper.emitted()).toHaveProperty('click');
   });
 
   it('Disabled', () => {
+    expect(Button).toBeTruthy();
+
     const wrapper = mount(Button, {
       props: {
         disabled: true,
       },
     });
-
-    expect(wrapper).toBeTruthy();
 
     expect(wrapper.classes()).toContain('disabled');
 
