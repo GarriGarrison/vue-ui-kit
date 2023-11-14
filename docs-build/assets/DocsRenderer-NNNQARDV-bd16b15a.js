00b2285829b04b8a6a643cb3a363b6694fa7cbd8
Garrison <garrison@pc103895.PKRVMN.local>
2023-11-14 15:03:14 +0300

    storybook: test publish

---
+++ docs-build/assets/DocsRenderer-NNNQARDV-bd16b15a.js
@@ -0,0 +1,2 @@
+import{D as s,d as a}from"./chunk-HLWAVYOI-8e8966d3.js";import"./iframe-6094de20.js";import"../sb-preview/runtime.js";import"./index-eb485722.js";import"./index-d37d4223.js";import"./index-356e4a49.js";export{s as DocsRenderer,a as defaultComponents};
+//# sourceMappingURL=DocsRenderer-NNNQARDV-bd16b15a.js.map
