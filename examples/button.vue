3bc699211ecf88aa4549d41a954c958dc56a7fa4
Garrison <garrison@pc103895.PKRVMN.local>
2023-11-15 18:19:16 +0300

    button: add example with App

---
+++ examples/button.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { GuvButton } from '@garrigarrison/vue-ui-kit';
+</script>
+
+<template>
+  <div class="wrapper">
+    <guv-button>
+      Click me
+      <template #after><img src="@/assets/search.svg" alt="image before" class="img" /></template>
+    </guv-button>
+
+    <guv-button class="test" />
+
+    <guv-button size="standard" />
+    <guv-button size="small" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.wrapper {
+  display: flex;
+  gap: 20px;
+
+  .img {
+    width: 15px;
+    height: 15px;
+  }
+}
+
+.test {
+  background-color: greenyellow;
+}
+</style>
