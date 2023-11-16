15790c2a1de8fd29843919710f4c04c82a87e6c3
Garrison <garrison@pc103895.PKRVMN.local>
2023-11-16 10:02:06 +0300

    storybook: return public folder storybook-static

--- docs/assets/WithTooltip-4HIR6TLV-535d9a38.js
+++
@@ -1,2 +0,0 @@
-import{W,W as T,a as e}from"./chunk-HLWAVYOI-8dc5e995.js";import"./iframe-e4c9a36e.js";import"../sb-preview/runtime.js";import"./index-eb485722.js";import"./index-d37d4223.js";import"./index-356e4a49.js";export{W as WithToolTipState,T as WithTooltip,e as WithTooltipPure};
-//# sourceMappingURL=WithTooltip-4HIR6TLV-535d9a38.js.map
