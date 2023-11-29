d9e41a9fd1de9d589480cc46c53da5aa4639da7b
Garrison <garrison@pc103895.PKRVMN.local>
2023-11-29 16:09:04 +0300

    input: add example

---
+++ examples/input.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { GuvInput } from '@garrigarrison/vue-ui-kit';
+
+const text = ref<string>();
+const num = ref<number>();
+const ups = ref();
+</script>
+
+<template>
+  <div class="wrapper">
+    <guv-input v-model="text" />
+
+    <guv-input v-model.number="num" type="number" />
+
+    <guv-input v-model.number="num" :mask="/^\d+$/" placeholder="mask" />
+
+    <guv-input v-model="text" disabled placeholder="disabled" />
+
+    <guv-input v-model="ups" invalid />
+
+    <guv-input v-model="text" :style="{ backgroundColor: 'beige' }" />
+
+    <guv-input v-model="text" class="test" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+
+  .img {
+    width: 15px;
+    height: 15px;
+  }
+}
+
+.test {
+  background-color: greenyellow;
+}
+</style>
