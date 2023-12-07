d51f85bfaf16b06c71740ceda7797db2134012fa
Garrison <garrison@pc103895.PKRVMN.local>
2023-12-07 10:32:33 +0300

    build: update package

--- vite.config.ts
+++ vite.config.ts
@@ -35,5 +35,13 @@ export default defineConfig({
 
   test: {
     environment: 'jsdom',
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/cypress/**',
+      '**/.{idea,git,cache,output,temp}/**',
+      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+      '**/storybook-static/**',
+    ],
   },
 });
